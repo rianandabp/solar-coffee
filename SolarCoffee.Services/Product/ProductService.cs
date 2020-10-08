1d32c54d0acb21e5865b96373e8ec2262c2cf7de
rianandabp <ryan.pitansa@gmail.com>
2020-10-09 00:57:51 +0700

    Impement Data Mapper

---
+++ SolarCoffee.Services/Product/ProductService.cs
@@ -0,0 +1,95 @@
+﻿using SolarCoffee.Data;
+using SolarCoffee.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SolarCoffee.Services.Product
+{
+    public class ProductService : IProductService
+    {
+        private readonly SolarDbContext _db;
+
+        public ProductService(SolarDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public  ServiceResponse<Data.Models.Product> ArchiveProduct(int id)
+        {
+            try
+            {
+                var product = _db.Products.Find(id);
+                product.IsArchived = true;
+                _db.SaveChanges();
+
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = "Archvied Product",
+                    IsSuccess = true
+                };
+            }
+            catch (Exception e)
+            {
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = null,
+                    Time = DateTime.UtcNow,
+                    Message = e.StackTrace,
+                    IsSuccess = false
+                };
+            }
+        }
+
+        public ServiceResponse<Data.Models.Product> CreateProduct(Data.Models.Product product)
+        {
+            try
+            {
+                _db.Products.Add(product);
+
+                var newInventory = new ProductInventory
+                {
+                    Product = product,
+                    QuantityOnHand = 0,
+                    IdealQuantity = 10
+                };
+
+                _db.ProductInventories.Add(newInventory);
+
+                _db.SaveChanges();
+
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = "Saved new product",
+                    IsSuccess = true
+                };
+            }
+            catch (Exception e)
+            {
+                return new ServiceResponse<Data.Models.Product>
+                {
+                    Data = product,
+                    Time = DateTime.UtcNow,
+                    Message = e.StackTrace,
+                    IsSuccess = false
+                };
+            }
+            
+        }
+
+        List<Data.Models.Product> IProductService.GetAllProducts()
+        {
+            return _db.Products.ToList();
+        }
+
+        Data.Models.Product IProductService.GetProductById(int id)
+        {
+            return _db.Products.Find(id);
+        }
+    }
+}
---
+++ SolarCoffee.Web/ViewModels/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolarCoffee.Web.ViewModels
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        [MaxLength(64)]
+        public string Name { get; set; }
+        [MaxLength(128)]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsTaxable { get; set; }
+        public bool IsArchived { get; set; }
+    }
+}
